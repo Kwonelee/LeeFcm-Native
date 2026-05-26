be6a4a15f26e815a4c197380b9895d051b5b87fe
Kwonelee <995546054@qq.com>
2026-05-26 14:10:10 +0800

    Update 03_prepare_package.sh

--- SCRIPTS/03_prepare_package.sh
+++ SCRIPTS/03_prepare_package.sh
@@ -8,6 +8,7 @@ sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/ut
 
 # samba4 default config
 sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/nas/services/g' feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
 
 # default LAN IP
 sed -i "s/192.168.1.1/192.168.5.88/g" package/base-files/files/bin/config_generate
