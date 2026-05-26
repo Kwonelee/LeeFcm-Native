ca57da625695af8c9842e555ab03dc1d05e3516a
Kwonelee <995546054@qq.com>
2026-05-26 11:17:35 +0800

    提交

---
+++ README.md
@@ -0,0 +1,3 @@
+## 🎉 Thanks
+- [lede](https://github.com/coolsnowwolf/lede) ； [fanchmwrt](https://github.com/fanchmwrt/fanchmwrt) ； [openwrt](https://github.com/openwrt/openwrt) ； [immortalwrt](https://github.com/immortalwrt/immortalwrt)
+- [FanchmWrt-25.12](https://github.com/Kwonelee/FanchmWrt-25.12) ； [fanchmwrt-packages](https://github.com/Kwonelee/fanchmwrt-packages)
---
+++ SCRIPTS/01_get_ready.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+# fanchmwrt处理
+sed -i 's/tty1::askfirst/tty1::respawn/g' target/linux/x86/base-files/etc/inittab
+cp -f $GITHUB_WORKSPACE/FILES/fcmfiles/banner package/base-files/files/etc/banner
+cp -f $GITHUB_WORKSPACE/FILES/fcmfiles/sysupgrade.conf package/base-files/files/etc/sysupgrade.conf
+cp -f $GITHUB_WORKSPACE/FILES/fcmfiles/950-fwx-nf-conn-struct-user-hook.patch target/linux/generic/hack-6.12/
+cp -f $GITHUB_WORKSPACE/FILES/fcmfiles/target.mk include/target.mk
+cp -f $GITHUB_WORKSPACE/FILES/fcmfiles/rc.local package/base-files/files/etc/rc.local
+cp -f $GITHUB_WORKSPACE/FILES/fcmfiles/Makefile package/base-files/Makefile
+cp -a $GITHUB_WORKSPACE/FILES/fcmfiles/fcm package/
+sed -i "s/hostname='LEDE'/hostname='FanchmWrt'/g" package/base-files/files/bin/config_generate
+#sed -i 's/default "LEDE"/default "FanchmWrt"/g' package/base-files/image-config.in
+sed -i 's/LEDE/OpenWrt/g' package/base-files/image-config.in
+sed -i 's/lede/openwrt/g' package/base-files/image-config.in
+sed -i 's/LEDE/OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+
+# fanchmwrt样式处理
+cp -f $GITHUB_WORKSPACE/FILES/fcmfiles/menu-fanchmwrt.js package/fcm/luci-theme-fanchmwrt/htdocs/luci-static/resources/menu-fanchmwrt.js
+cp -rf $GITHUB_WORKSPACE/FILES/fanchmwrt/* package/fcm/luci-theme-fanchmwrt/htdocs/luci-static/fanchmwrt/
+
+# 使用6.18内核
+#sed -i "s/6.12/6.18/g" target/linux/rockchip/Makefile
