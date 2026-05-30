34f10875c640a6bf0d081a6fbc1bfda4475fa6fa
Kwonelee <995546054@qq.com>
2026-05-30 11:12:59 +0800

    Update menu-fanchmwrt.js

--- FILES/fcmfiles/menu-fanchmwrt.js
+++ FILES/fcmfiles/menu-fanchmwrt.js
@@ -158,8 +158,7 @@ return baseclass.extend({
 					'type': 'button',
 					'aria-label': _('Mode menu')
 				}, [
-					E('span', { 'class': 'top-more-icon' }, [
-						E('img', { 'src': '/luci-static/fanchmwrt/icons/fwx_model.png', 'style': 'width: 25px; height: 25px;' })
+					E('span', { 'class': 'top-more-icon' }, [E('img', { 'src': '/luci-static/fanchmwrt/icons/fwx_model.png', 'style': 'width: 25px; height: 25px;' })])
 				]),
 				E('ul', { 'class': 'top-user-dropdown top-mode-dropdown' }, [
 					createModeItem('basic', 'Normal Mode', L.url('admin', 'fwx_dashboard')),
