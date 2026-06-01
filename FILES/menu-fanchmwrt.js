0fe98bf1f8afcb28b201cedc1680fa2f606fd8ed
Kwonelee <995546054@qq.com>
2026-06-01 19:53:57 +0800

    Update menu-fanchmwrt.js

--- FILES/menu-fanchmwrt.js
+++ FILES/menu-fanchmwrt.js
@@ -279,10 +279,7 @@ return baseclass.extend({
 				if (i === 0 && pathSegment === 'admin') {
 					const homeItem = E('li', { 'class': 'home-item' }, [
 						E('a', { 'href': '/cgi-bin/luci/admin/fwx_dashboard', 'title': _('Home') }, [
-							E('img', {
-							    'src': '/luci-static/fanchmwrt/icons/home.png',  // 👈 PNG 图标路径
-								'style': 'width: 20px; height: 20px; vertical-align: middle;'
-							 })
+							E('span', { 'class': 'home-icon' })
 						])
 					]);
 					breadcrumbItems.push(homeItem);
