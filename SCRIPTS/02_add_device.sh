fbabd639a3f5cab919d7a9ec34df9c5932776980
Kwonelee <995546054@qq.com>
2026-06-26 13:45:18 +0800

    Update 02_add_device.sh

--- SCRIPTS/02_add_device.sh
+++ SCRIPTS/02_add_device.sh
@@ -31,12 +31,12 @@ cp -f $GITHUB_WORKSPACE/FILES/uboot-rockchip/Makefile package/boot/uboot-rockchi
 # 复制uboot配置、dts到package/boot/uboot-rockchip
 mkdir -p package/boot/uboot-rockchip/src/arch/arm/dts
 mkdir -p package/boot/uboot-rockchip/src/configs
-cp -f $GITHUB_WORKSPACE/FILES/dts/rk3566-roc-pc.dts package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/FILES/uboot-rockchip/rk3566-station-m2-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/FILES/dts/*.dts package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/FILES/uboot-rockchip/*.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
 cp -f $GITHUB_WORKSPACE/FILES/uboot-rockchip/station-m2-rk3566_defconfig package/boot/uboot-rockchip/src/configs/
 
 # 复制dts到files/arch/arm64/boot/dts/rockchip
 mkdir -p target/linux/rockchip/files/arch/arm64/boot/dts/rockchip
-cp -f $GITHUB_WORKSPACE/FILES/dts/rk3566-roc-pc.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
+cp -f $GITHUB_WORKSPACE/FILES/dts/*.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
 # 添加设备⬆⬆⬆
 # ============================================================================================================
